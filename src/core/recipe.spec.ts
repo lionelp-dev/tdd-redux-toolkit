365434bf6a6e39b9550f92b30013972e4bf95a56
lionelp.dev <lionelp.dev@gmail.com>
2022-02-26 13:14:08 +0100

    feat: recipe should return an empty list of result

--- src/core/recipe.spec.ts
+++ src/core/recipe.spec.ts
@@ -1,3 +1,35 @@
-describe("Recipe", () => {});
+import {
+  createSlice,
+  configureStore,
+  createEntityAdapter,
+} from "@reduxjs/toolkit";
+
+export interface Recipes {
+  id: string;
+  name: string;
+}
+
+const recipesAdapter = createEntityAdapter<Recipes>();
+
+const recipesSlice = createSlice({
+  name: "recipes",
+  initialState: recipesAdapter.getInitialState(),
+  reducers: {},
+});
+
+describe("Recipe", () => {
+  it("should return an empty list of recipe", () => {
+    const store = configureStore({
+      reducer: {
+        recipes: recipesSlice.reducer,
+      },
+    });
+    type rootState = ReturnType<typeof store.getState>;
+    const recipesSelectors = recipesAdapter.getSelectors<rootState>((state) => {
+      return state.recipes;
+    });
+    expect(recipesSelectors.selectAll(store.getState())).toEqual([]);
+  });
+});
 
 export {};
