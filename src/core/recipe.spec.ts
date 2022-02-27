d0f08866ef8ce5a9f71675410de796da43cc7913
lionelp.dev <lionelp.dev@gmail.com>
2022-02-27 10:10:27 +0100

    feat ensure selectAllRecipes can retrive a list of many recipes

--- src/core/recipe.spec.ts
+++ src/core/recipe.spec.ts
@@ -2,33 +2,85 @@ import {
   createSlice,
   configureStore,
   createEntityAdapter,
+  createAsyncThunk,
 } from "@reduxjs/toolkit";
 
 export interface Recipes {
-  id: string;
+  id: number;
   name: string;
 }
 
 const recipesAdapter = createEntityAdapter<Recipes>();
 
+export const fetchRecipes = createAsyncThunk(
+  "recipes/fetchRecipes",
+  async (recipes: any) => {
+    return {
+      recipes,
+    };
+  }
+);
+
 const recipesSlice = createSlice({
   name: "recipes",
   initialState: recipesAdapter.getInitialState(),
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchRecipes.fulfilled, (state, action) => {
+      recipesAdapter.setAll(state, action.payload.recipes);
+    });
+  },
 });
+// First, create the thunk
+const makeSut = () => {
+  const store = configureStore({
+    reducer: {
+      recipes: recipesSlice.reducer,
+    },
+  });
+  type rootState = ReturnType<typeof store.getState>;
+  const recipesSelectors = recipesAdapter.getSelectors<rootState>((state) => {
+    return state.recipes;
+  });
+  const selectAllRecipes = () => {
+    return recipesSelectors.selectAll(store.getState());
+  };
+  return {
+    store,
+    recipesSelectors,
+    selectAllRecipes,
+  };
+};
 
 describe("Recipe", () => {
   it("should return an empty list of recipe", () => {
-    const store = configureStore({
-      reducer: {
-        recipes: recipesSlice.reducer,
+    const { store, selectAllRecipes } = makeSut();
+    expect(selectAllRecipes()).toEqual([]);
+  });
+  it("should return a list of many recipes", async () => {
+    const { store, selectAllRecipes } = makeSut();
+    await store.dispatch(
+      fetchRecipes([
+        {
+          id: "1",
+          name: "columbo",
+        },
+        {
+          id: "2",
+          name: "columbo",
+        },
+      ])
+    );
+    expect(selectAllRecipes()).toEqual([
+      {
+        id: "1",
+        name: "columbo",
       },
-    });
-    type rootState = ReturnType<typeof store.getState>;
-    const recipesSelectors = recipesAdapter.getSelectors<rootState>((state) => {
-      return state.recipes;
-    });
-    expect(recipesSelectors.selectAll(store.getState())).toEqual([]);
+      {
+        id: "2",
+        name: "columbo",
+      },
+    ]);
   });
 });
 
